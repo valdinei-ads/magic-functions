93806ed3c0d51f449d9d8fb4985a0cebb710ca49
Valdinei Sacramento de Almeida <valdinei.almeida@consulting-for.edenred.com>
2024-09-18 21:01:16 -0300

    Implementacao da funcao bestRatedFilm

--- src/bestRatedFilm.js
+++ src/bestRatedFilm.js
@@ -2,6 +2,14 @@ const movies = require("../data/movies");
 
 const bestRatedFilm = (movies) => {
   
+  if (movies === undefined || movies === null || movies.length === 0) return undefined; 
+
+  var result = movies.reduce((melhor, filme) =>  melhor.avaliacao > filme.avaliacao 
+    ? melhor 
+    : filme
+  );
+  
+  return result;
 }
 
 module.exports = {
