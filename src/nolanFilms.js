4db0ca0aede3e88bf1130080fdd2c351d66eaf56
Valdinei Sacramento de Almeida <valdinei.almeida@consulting-for.edenred.com>
2024-09-19 14:43:29 -0300

    Implementacao da funcao nolanFilms

--- src/nolanFilms.js
+++ src/nolanFilms.js
@@ -1,7 +1,8 @@
 const movies = require("../data/movies");
 
 const nolanFilms = (movies) => {
- 
+  return movies.filter((item) => item.diretor.includes('Nolan') )
+        .sort((a, b) => a.avaliacao > b.avaliacao );
 }
 
 module.exports = {
